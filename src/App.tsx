1f05eba1ef05f2dc735f623d8537eb066a6dca99
Renan Matias <rnmatias@hotmail.com>
2025-09-13 22:44:45 +0000

    feat: implement Heading component with styles for improved UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Heading } from "./components/Heading";
+
+import "./styles/theme.css";
+import "./styles/global.css";
+
 export function App() {
   console.log("App component rendered");
 
   return (
     <>
-      <h1>Hello, world!</h1>
+      <Heading />
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore quae vero illum optio ea. Veritatis dolores,
         minima perferendis facilis aliquid praesentium, et ipsam earum harum molestiae consequatur excepturi, cupiditate
