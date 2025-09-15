2824a61d062dd9abebeb9bd356129a34fb0dfe63
Renan Matias <rnmatias@hotmail.com>
2025-09-15 23:11:07 +0000

    refactor: remove Heading component and restructure App layout for clarity

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import { Heading } from "./components/Heading";
-
 import "./styles/theme.css";
 import "./styles/global.css";
-import { TimerIcon } from "lucide-react";
 
 export function App() {
   console.log("App component rendered");
 
   return (
     <>
-      <Heading>
-        Hello, world!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore quae vero illum optio ea. Veritatis dolores,
-        minima perferendis facilis aliquid praesentium, et ipsam earum harum molestiae consequatur excepturi, cupiditate
-        beatae!
-      </p>
+      <div className="container">
+        <div className="content">
+          <section>LOGO</section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>MENU</section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>FORM</section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>FOOTER</section>
+        </div>
+      </div>
     </>
   );
 }
