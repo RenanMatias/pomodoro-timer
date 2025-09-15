d25449f68b9bd20f90213e6143f2680b89705f4f
Renan Matias <rnmatias@hotmail.com>
2025-09-15 00:43:31 +0000

    feat: enhance Heading component with children prop and add `TimerIcon` button

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import { Heading } from "./components/Heading";
 
 import "./styles/theme.css";
 import "./styles/global.css";
+import { TimerIcon } from "lucide-react";
 
 export function App() {
   console.log("App component rendered");
 
   return (
     <>
-      <Heading />
+      <Heading>
+        Hello, world!
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore quae vero illum optio ea. Veritatis dolores,
         minima perferendis facilis aliquid praesentium, et ipsam earum harum molestiae consequatur excepturi, cupiditate
