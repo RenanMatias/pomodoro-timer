2d72ad3ee23953ef159fb564196b204421b88549
Renan Matias <rnmatias@hotmail.com>
2025-09-17 00:10:44 +0000

    feat: implement Menu component and integrate into App layout fix: update hover brightness for logo and menu links

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Container } from "./components/Container";
 import { Heading } from "./components/Heading";
 import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
 
 import "./styles/theme.css";
 import "./styles/global.css";
@@ -11,12 +12,15 @@ export function App() {
       <Container>
         <Logo />
       </Container>
+
       <Container>
-        <Heading>MENU</Heading>
+        <Menu />
       </Container>
+
       <Container>
         <Heading>FORM</Heading>
       </Container>
+
       <Container>
         <Heading>FOOTER</Heading>
       </Container>
