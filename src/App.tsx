47f6e9663056e65c628d858c84276cd67635baf1
Renan Matias <rnmatias@hotmail.com>
2025-09-17 00:47:13 +0000

    feat: add  component with form structure in App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Container } from "./components/Container";
 import { CountDown } from "./components/CountDown";
-import { Heading } from "./components/Heading";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 
@@ -23,7 +22,25 @@ export function App() {
       </Container>
 
       <Container>
-        <Heading>FOOTER</Heading>
+        <form className="form" action="">
+          <div className="formRow">
+            <label htmlFor="input">task</label>
+            <input id="input" type="text" />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
