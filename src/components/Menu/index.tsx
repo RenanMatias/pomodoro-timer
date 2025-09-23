2a0345fbe5df950e035659a950e5518f96eb0fa1
Renan Matias <rnmatias@hotmail.com>
2025-09-23 01:33:13 +0000

    feat: implement theme toggle functionality in Menu component

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,22 +1,34 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
 import styles from "./styles.module.css";
+import { useState } from "react";
+
+type AvailableThemes = "dark" | "light";
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>("dark");
+
+  function handleThemeChange(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    event.preventDefault(); // Don't follow the link
+
+    console.log("Clicado", Date.now());
+  }
+
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href="#">
+      <h1>{theme}</h1>
+      <a className={styles.menuLink} href="#" aria-label="Home" title="Home">
         <HouseIcon />
       </a>
 
-      <a className={styles.menuLink} href="#">
+      <a className={styles.menuLink} href="#" aria-label="Histórico" title="Histórico">
         <HistoryIcon />
       </a>
 
-      <a className={styles.menuLink} href="#">
+      <a className={styles.menuLink} href="#" aria-label="Configurações" title="Configurações">
         <SettingsIcon />
       </a>
 
-      <a className={styles.menuLink} href="#">
+      <a className={styles.menuLink} href="#" aria-label="Mudar tema" title="Mudar tema" onClick={handleThemeChange}>
         <SunIcon />
       </a>
     </nav>
