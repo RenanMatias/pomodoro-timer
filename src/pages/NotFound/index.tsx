a4fd9310d8be774105e43203d595c84136becad1
Renan Matias <rnmatias@hotmail.com>
2025-09-25 01:24:17 +0000

    feat: Create Home page and a template for all other pages

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,17 @@
+import { Container } from "../../components/Container";
+import { MainTemplate } from "../../templates/MainTemplate";
+
+export function NotFound() {
+  return (
+    <MainTemplate>
+      <Container>
+        <h1>Página não encontrada</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quam a totam vel. Sit voluptatum qui
+          cupiditate quod accusantium reiciendis est reprehenderit eaque quis saepe blanditiis eligendi, consequuntur
+          expedita. Minus.
+        </p>
+      </Container>
+    </MainTemplate>
+  );
+}
