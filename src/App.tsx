f5a50df2be3650eb6d5fb286ed28e9f22648ec78
Matias <rnmatias@hotmail.com>
2025-12-22 16:10:42 -0300

    Context API - `useContext` + `useState`

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,12 @@ import { Home } from "./pages/Home";
 
 import "./styles/theme.css";
 import "./styles/global.css";
-import { useState } from "react";
-import type { TaskStateModel } from "./models/TaskStateModel";
-import { TaskContext } from "./contexts/TaskContext";
-
-const initialState: TaskStateModel = {
-  tasks: [],
-  secondsRemaining: 0,
-  formatedSecondsRemaining: "00:00",
-  activeTask: null,
-  currentCycle: 0,
-  config: {
-    workTime: 25,
-    shortBreakTime: 5,
-    longBreakTime: 15,
-  },
-};
+import { TaskContextProvider } from "./contexts/TaskContext";
 
 export function App() {
-  const [state, setState] = useState(initialState);
-
   return (
-    <TaskContext.Provider value={{ outraCoisa: 321 }}>
-      <Home />;
-    </TaskContext.Provider>
+    <TaskContextProvider>
+      <Home />
+    </TaskContextProvider>
   );
 }
