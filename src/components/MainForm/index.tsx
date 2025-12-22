5445b02992d67b16827ae3da367dcb889fd4a8e6
Matias <rnmatias@hotmail.com>
2025-12-22 15:30:46 -0300

    Context API - create a context with `createContext` + `Provider`

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,38 +1,17 @@
-import { PlayCircleIcon } from "lucide-react";
+import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
+import { PlayCircleIcon } from "lucide-react";
 
-import { Cycles } from "../Cycles";
-import type { HomeProps } from "../../pages/Home";
-
-export function MainForm({ state, setState }: HomeProps) {
-  function handleClick() {
-    setState(prevState => {
-      return {
-        ...prevState,
-        config: {
-          ...prevState.config,
-          workTime: 34,
-        },
-        formatedSecondsRemaining: "23:34",
-      };
-    });
-  }
-
+export function MainForm() {
   return (
     <form className="form" action="">
-      <div>
-        <button type="button" onClick={handleClick}>
-          Clicar
-        </button>
-      </div>
-
       <div className="formRow">
         <DefaultInput labelText="task" id="meuInput" type="text" placeholder="Digite algo" />
       </div>
 
       <div className="formRow">
-        <p>Próximo intervalo é de {state.config.workTime} minutos</p>
+        <p>Próximo intervalo é de 25 minutos</p>
       </div>
 
       <div className="formRow">
