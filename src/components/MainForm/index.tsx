f6d6f740eb389b727f481d5fde9d117512a8085c
Matias <rnmatias@hotmail.com>
2025-12-23 10:51:17 -0300

    +  to not cause re-render in the components

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,29 +1,21 @@
-import { useState } from "react";
+import { useRef } from "react";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
 import { PlayCircleIcon } from "lucide-react";
 
 export function MainForm() {
-  const [taskName, setTaskName] = useState("");
+  const taskNameInput = useRef<HTMLInputElement>(null);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-
-    console.log("DEU CERTO", taskName);
+    console.log("DEU CERTO");
   }
 
   return (
     <form onSubmit={handleCreateNewTask} className="form" action="">
       <div className="formRow">
-        <DefaultInput
-          labelText="task"
-          id="meuInput"
-          type="text"
-          placeholder="Digite algo"
-          value={taskName}
-          onChange={e => setTaskName(e.target.value)}
-        />
+        <DefaultInput labelText="task" id="meuInput" type="text" placeholder="Digite algo" ref={taskNameInput} />
       </div>
 
       <div className="formRow">
